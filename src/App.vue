56bf6923c9d5caa02ea8b30c278d021cfbe495dd
MarcAntoineComte <marcantoine.comte@bell.ca>
2022-06-13 15:47:40 -0400

    pokemon-app
    - Add translations with vue-i18n

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="container my-5">
-    <div class="offset-8 row">
-        <a class="col-3" href="#/load-more">Load More</a> |
-        <a class="col" href="#/pagination">Pagination</a>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ t("pokemon-app") }}</a>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              {{t("navigation-type")}}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="btn btn-light border-bottom-secondary" href="#/load-more">{{ t("load-more") }}</a></li>
+              <li><a class="btn btn-light" href="#/pagination">{{ t("pagination") }}</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="container my-5">
     <div class="ms-2">
       <component :is="currentNavigation"/>
     </div>
@@ -13,12 +27,22 @@
 <script>
 import LoadMoreListing from './components/navigations/LoadMoreListing.vue'
 import PaginationListing from './components/navigations/PaginationListing.vue'
+import { useI18n } from 'vue-i18n'
+
 const routes = {
   '/pagination': PaginationListing,
   '/load-more': LoadMoreListing
 }
 export default {
   name: 'App',
+  setup() {
+    const { locale, t } = useI18n({
+      inheritLocale: true,
+    })
+    return {
+      locale, t
+    }
+  },
   components: {
     LoadMoreListing, PaginationListing
   },
--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import { createI18n } from 'vue-i18n'
 
 var app = createApp(App)
+const i18n = new createI18n({
+    legacy: false,
+    locale: 'en',
+    messages: {
+      'en': {
+        'pokemon-app': 'Pokemon Test Challenge',
+        'load-more': 'Load More',
+        'pagination': 'Pagination',
+        'navigation-type': 'Navigation Type'
+      },
+    }
+  })
 
+app.use(i18n);
 app.use(VueAxios, axios)
 app.mount('#app')
